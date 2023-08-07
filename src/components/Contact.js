110a03e0901bef8337dac54b1e4bffae26b1e2e7
panavsethi1 <panav.sethi.delhi@gmail.com>
2023-08-07 18:20:35 +0530

    build: added sheet as backend for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 
 function Contact() {
+	const [contactData, setContactData] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
+
+	const [disable, setDisable] = useState(false);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setDisable(true);
+		if (
+			!contactData.name.length ||
+			!contactData.email.length ||
+			!contactData.message.length
+		) {
+			alert('Please fill in all the details!');
+			setDisable(false);
+		} else if (
+			!contactData.email
+				.toLowerCase()
+				.match(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+				)
+		) {
+			alert('Please fill in a valid email address!');
+			setDisable(false);
+		} else {
+			Axios.post(
+				'https://bored-goat-bedclothes.cyclic.app/contact',
+				{ data: contactData },
+				{
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			).then((res) => {
+				if (res.status === 201 || res.status === 200) {
+					setDisable(false);
+					alert("Message successfully sent! We'll get back to you asap.");
+					setContactData({ name: '', email: '', message: '' });
+				} else {
+					setDisable(false);
+					alert('Error in sending your message! Please try again.');
+					setContactData({ name: '', email: '', message: '' });
+				}
+			});
+		}
+	};
+
 	return (
 		<section id='contact' className='contact'>
 			<Container>
@@ -11,17 +61,50 @@ function Contact() {
 					<Col lg={6} md={8} sm={12}>
 						<Form.Group className='mb-3'>
 							<Form.Label>Name</Form.Label>
-							<Form.Control type='text' />
+							<Form.Control
+								onChange={(e) => {
+									setContactData((prev) => {
+										return { ...prev, name: e.target.value };
+									});
+								}}
+								type='text'
+								value={contactData.name}
+							/>
 						</Form.Group>
 						<Form.Group className='mb-3'>
 							<Form.Label>Email</Form.Label>
-							<Form.Control type='email' />
+							<Form.Control
+								onChange={(e) => {
+									setContactData((prev) => {
+										return { ...prev, email: e.target.value };
+									});
+								}}
+								type='email'
+								value={contactData.email}
+							/>
 						</Form.Group>
 						<Form.Group className='mb-3'>
 							<Form.Label>Message</Form.Label>
-							<Form.Control as='textarea' type='text' />
+							<Form.Control
+								as='textarea'
+								onChange={(e) => {
+									setContactData((prev) => {
+										return { ...prev, message: e.target.value };
+									});
+								}}
+								type='text'
+								value={contactData.message}
+							/>
 						</Form.Group>
-						<a href='/'>Submit</a>
+						<a
+							style={{
+								backgroundColor: disable ? '#dddddd' : '#f94f03',
+								pointerEvents: disable ? 'none' : 'all',
+							}}
+							onClick={handleSubmit}
+							href='/'>
+							Submit
+						</a>
 					</Col>
 				</Row>
 			</Container>
